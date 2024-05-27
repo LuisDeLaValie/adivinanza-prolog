eac6fa5f1266c8ba7bbf6a81f89761e183af1ab4
TDTxLE <TDTxLE@gmail.com>
2024-05-27 07:47:00 -0600

    quitar asentos

--- adivinanza.pl
+++ adivinanza.pl
@@ -1,13 +1,13 @@
 % Lista de adivinanzas y sus respuestas
 adivinanza("Blanco por dentro, verde por fuera. Si quieres que te lo diga, espera.", sandia).
-adivinanza("De muchos colores me verás, por el campo voy y no ando jamás.", arcoiris).
-adivinanza("Con cincuenta ojos y una lengua, siempre está cerca de una fragua.", abanico).
-adivinanza("Tengo agujas pero no sé coser. Tengo números pero no sé leer.", reloj).
+adivinanza("De muchos colores me veras, por el campo voy y no ando jamas.", arcoiris).
+adivinanza("Con cincuenta ojos y una lengua, siempre esta cerca de una fragua.", abanico).
+adivinanza("Tengo agujas pero no se coser. Tengo numeros pero no se leer.", reloj).
 adivinanza("Con cien patas y no puede andar, con mil ojos y no puede mirar.", abanico).
-adivinanza("Vuelo sin alas, canto sin voz, tengo un nido pero no soy un pájaro.", nube).
+adivinanza("Vuelo sin alas, canto sin voz, tengo un nido pero no soy un pajaro.", nube).
 adivinanza("Luzco siempre en compañía, en el cielo o en el mar, me puedes ver brillar, pero no me puedes tocar.", estrella).
-adivinanza("No es cama ni es león y desaparece en cualquier rincón.", camaleon).
-adivinanza("Vuelo de noche, duermo en el día y nunca verás plumas en ala mía.", murcielago).
+adivinanza("No es cama ni es leon y desaparece en cualquier rincon.", camaleon).
+adivinanza("Vuelo de noche, duermo en el dia y nunca veras plumas en ala mia.", murcielago).
 adivinanza("Pequeña como una pera, pero alumbra toda la casa entera.", bombilla).
 
 
@@ -27,7 +27,7 @@ jugar_adivinanza :-
     writeln("¡Bienvenido al juego de adivinanzas!"),
     seleccionar_adivinanza(Adivinanza, Respuesta),
     writeln(Adivinanza),
-    writeln("¿Cuál es tu respuesta?"),
+    writeln("¿Cual es tu respuesta?"),
     read(RespuestaUsuario),
     validar_respuesta(RespuestaUsuario, Respuesta).
 
@@ -41,7 +41,7 @@ validar_respuesta(RespuestaUsuario, RespuestaCorrecta) :-
     !.
 validar_respuesta(_, RespuestaCorrecta) :-
     writeln("¡Incorrecto! ¡Inténtalo de nuevo!"),
-    writeln("Aquí tienes una pista: la respuesta tiene "),
+    writeln("Aqui tienes una pista: la respuesta tiene "),
     atom_length(RespuestaCorrecta, Length),
     write(Length),
     writeln(" letras"),
